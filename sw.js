49b2d227d3565b60fb62a0f34936ea08ff7e169a
macklij74 <jason.s.macklin@gmail.com>
2026-04-01 11:46:43 -0400

    Update service worker cache version and asset paths
    In an attempt to get GPS functionality added to the map view.

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE = 'dpnc-trails-v1';
+const CACHE = 'dpnc-trails-v2';
 const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icon-192.svg',
-  './icon-512.svg',
+  '/dpnc-trail-map/',
+  '/dpnc-trail-map/index.html',
+  '/dpnc-trail-map/manifest.json',
+  '/dpnc-trail-map/icon-192.svg',
+  '/dpnc-trail-map/icon-512.svg',
   'https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700;800;900&family=Lora:ital,wght@0,500;1,400&display=swap'
 ];
 
@@ -28,6 +28,6 @@ self.addEventListener('fetch', e => {
       const clone = res.clone();
       caches.open(CACHE).then(cache => cache.put(e.request, clone));
       return res;
-    }).catch(() => caches.match('./index.html')))
+    }).catch(() => caches.match('/dpnc-trail-map/index.html')))
   );
 });
